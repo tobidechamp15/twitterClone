4a9cad0395158d61b7b1e23ba494f81fcc216dac
unknown <tobidechamp15@gmail.com>
2023-07-12 10:12:21 +0100

    W-I-P

--- script.js
+++ script.js
@@ -1,12 +1,57 @@
 function toggleSidebar() {
   var sidebar = document.getElementById("sidebar");
   sidebar.classList.toggle("active");
+  // var overlay = document.getElementById("overlay");
+  // overlay.style.display = "block";
 }
 const menu = document.querySelector(".user-img");
 menu.addEventListener("click", toggleSidebar);
 
-const cancel = document.querySelector(".cancel")
+const cancel = document.querySelector(".cancel");
 cancel.addEventListener("click", function () {
-    var sidebar = document.getElementById("sidebar");
-    sidebar.classList.remove("active");
+  var sidebar = document.getElementById("sidebar");
+  sidebar.classList.remove("active");
+  // var overlay = document.getElementById("overlay");
+  // overlay.style.display = "none";
+});
+
+const screens = document.querySelector(".screen");
+screens.addEventListener("click", function () {
+  var sidebar = document.getElementById("sidebar");
+  sidebar.classList.remove("active");
+});
+const screns = document.querySelector(".scren");
+screns.addEventListener("click", function () {
+  var sidebar = document.getElementById("sidebar");
+  sidebar.classList.remove("active");
+});
+
+const bird = document.querySelector(".bird", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
+
+
+
+const posts=document.querySelector("body");
+posts.addEventListener("keypress",(e)=>{
+  console.log(e.key);
 })
+
+
+
+
+
+
+// const topBar = document.querySelector("#mySection")
+// topBar.addEventListener("mouseover", () => {
+//   const section = document.querySelector("nav")
+//   section.classList.remove("hide")
+//   section.classList.add("trans")
+// })
+// topBar.addEventListener("mouseout", () => {
+//   const section = document.querySelector("nav");
+//   section.classList.add("hide");
+// });
