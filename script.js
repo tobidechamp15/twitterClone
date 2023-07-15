3b7f02c2c77824fa17a1947c52a150ecdfbfbca6
Olubukola Oluwadare <Olubukola.Oluwadare@ubagroup.com>
2023-07-15 05:13:49 +0100

    Tweet-page

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function toggleSidebar() {
   var sidebar = document.getElementById("sidebar");
   sidebar.classList.toggle("active");
-  const homePage = document.querySelector(".home")
+  const homePage = document.querySelector(".home");
   homePage.style.backgroundColor = "rgb(63, 61, 61)";
   // var overlay = document.getElementById("overlay");
   // overlay.style.display = "block";
@@ -39,28 +39,59 @@ const bird = document.querySelector(".bird", () => {
   });
 });
 
-
-
-const posts=document.querySelector("body");
-posts.addEventListener("keypress",(e)=>{
+const posts = document.querySelector("body");
+posts.addEventListener("keypress", (e) => {
   console.log(e.key);
-})
+});
 
-$
-
+$;
 
 // jquery for beginner
 $(document).ready(function () {
-    $(".menu-btn").click(function () {
-        $(".sidebar").toggleClass("active");
-      });
+  $(".menu-btn").click(function () {
+    $(".sidebar").toggleClass("active");
+  });
 });
-    
 
 //function to store in local storage
 
 function store(key, value) {
-  localStorage.setItem(key, value)
+  localStorage.setItem(key, value);
 }
- 
 
+$(document).ready(function () {
+  $("#flip").click(function () {
+    $("#panel").addClass("slide-up");
+    $("#panel").slideToggle(900);
+  });
+  $("#tweet-cancel").click(() => {
+    $("#panel").slideToggle(900);
+  });
+});
+const active = $(".tweet-type").css("bottom");
+console.log(active);
+if (active == "-365.594px") {
+  $(".tweet-page").css("background-color", "rgb(63, 61, 61)");
+} else if (active == "0px") {
+  $(".tweet-page").css("background-color", "black");
+}
+$(".drop-down").click(() => {
+  $(".tweet-type").toggleClass("tweet-type-active");
+  // const homePage = document.querySelector(".home");
+  // homePage.style.backgroundColor = "rgb(63, 61, 61)";
+  // $(".tweet-page").css("background-color", "rgb(63, 61, 61)");
+  const active = $(".tweet-type").css("bottom");
+  console.log(active);
+  if (active == "-365.594px") {
+    $(".tweet-page").css("background-color", "rgb(63, 61, 61)");
+  } else if (active == "0px") {
+    $(".tweet-page").css("background-color", "black");
+  }
+});
+$(document).ready(function () {
+  $('input[name="myRadio"]').change(function () {
+     $("#result").text("Everyone");
+    var selectedValue = $('input[name="myRadio"]:checked').val();
+    $("#selectedValue").text(selectedValue);
+  });
+});
